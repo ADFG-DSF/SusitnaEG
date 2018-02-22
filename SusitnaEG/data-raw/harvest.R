654fa9a3eb46735f7660533694d66a72065b6ad5
Reimer <adam.reimer@dfg.alaska.local>
2018-02-22 14:27:06 -0900

    Harvest data by group, rename files to match steves convention, include missing groups in harvest and survey files.

--- SusitnaEG/data-raw/harvest.R
+++ SusitnaEG/data-raw/harvest.R
@@ -1,15 +1,14 @@
-survey_raw <-
+harvest_raw <-
 readxl::read_excel(".\\SusitnaEG\\data-raw\\Susitna run reconstruction data_Jan102018.xlsx",
-                   range = "Aerial counts!A3:AO25") %>%
-  dplyr::rename(group = Group, trib = Tributary) %>%
-  dplyr::filter(!grepl("weir|Weir", trib),                #drop Deshka Weir counts
-                !grepl("non-hatch", trib),                #drop Deception Creek non-hatchery
-                !grepl("Other WS", trib),                 #drop porrly defined surveys
-                !grepl("Other ES", trib)) %>%            
-  tidyr::gather(year, count, -group, -trib) 
+                   range = "Harvest!A1:AO20",
+                   col_names = TRUE) %>%
+  dplyr::rename(group = Group, trib = X__1) %>%
+  dplyr::filter(!is.na(group)) %>%
+  tidyr::gather(year, count, -group, -trib) %>%
+  dplyr::filter(as.numeric(year) >= 1979)
 
-#groups E, F, G and K have some years with partial counts within the group
-survey_raw %>%
+#groups B, E, K and N have some years with partial counts within the group
+harvest_raw %>%
   dplyr::group_by(year, group) %>%
   dplyr::summarise(sum = sum(count, na.rm = TRUE),
                    counts = n(),
@@ -21,30 +20,30 @@ survey_raw %>%
     ggplot2::scale_x_continuous(breaks = function(x) unique(floor(pretty(seq(0, (max(x) + 1) * 1.1)))))
 
 #proportions for each trib by group
-reflevels <- sapply(unique(survey_raw$group), function(x) sort(survey_raw$trib[survey_raw$group == x])[1])
-mlreg <- function(group_c){
+reflevels <- sapply(unique(harvest_raw$group), function(x) sort(harvest_raw$trib[harvest_raw$group == x])[1])
+mlreg <- function(dat, group_c){
   dat_reg <-
-  survey_raw %>%
-    dplyr::left_join(survey_raw %>%
+  dat %>%
+    dplyr::left_join(dat %>%
                        dplyr::group_by(year, group) %>%
                        dplyr::summarise(total = sum(count)), 
                      c("year", "group")) %>%
     na.omit() %>%
     dplyr::filter(group == group_c) %>%
     dplyr::mutate(trib2 = relevel(as.factor(trib), ref = reflevels[group_c]),
-                  year2 = as.numeric(year) - mean(as.numeric(unique(survey_raw$year))))
+                  year2 = as.numeric(year) - mean(as.numeric(unique(dat$year))))
   
   test2 <- nnet::multinom(trib2 ~ year2, data = dat_reg, weights = count)
   
   z <- summary(test2)$coefficients/summary(test2)$standard.errors
   wald <- (1 - pnorm(abs(z), 0, 1)) * 2
   preds <- 
-    predict(test2, newdata = data.frame(year2 = 1979:2017 - mean(as.numeric(unique(survey_raw$year)))), type = "probs") %>%
+    predict(test2, newdata = data.frame(year2 = 1979:2017 - mean(as.numeric(unique(dat$year)))), type = "probs") %>%
     as.data.frame() %>%
     dplyr::mutate(year = 1979:2017) %>%
     tidyr::gather(trib, prob, -year)
-  if(length(unique(survey_raw$trib[survey_raw$group == group_c])) == 2){
-    preds$trib <- unique(survey_raw$trib[survey_raw$group == group_c])[unique(survey_raw$trib[survey_raw$group == group_c]) != reflevels[group_c]]
+  if(length(unique(dat$trib[dat$group == group_c])) == 2){
+    preds$trib <- unique(dat$trib[dat$group == group_c])[unique(dat$trib[dat$group == group_c]) != reflevels[group_c]]
     preds2 <- preds
     preds2$trib <- reflevels[group_c]
     preds2$prob <- 1 - preds$prob
@@ -58,35 +57,30 @@ mlreg <- function(group_c){
   
   list(reg = test2, wald = wald, preds = preds, plot = plot)
 }
-regE <- mlreg("E")
+regB <- mlreg(harvest_raw, "B")
+summary(regB[[1]])
+regB[[4]]
+
+regE <- mlreg(harvest_raw, "E")
 summary(regE[[1]])
 regE[[4]]
 
-regF <- mlreg("F")
-summary(regF[[1]])
-regF[[4]]
+regN <- mlreg(harvest_raw, "N")
+summary(regN[[1]])
+regN[[4]]
 
-regG <- mlreg("G")
-summary(regG[[1]])
-regG[[4]]
+preds <- rbind(regB[[3]],
+               regE[[3]],
+               regN[[3]])
 
-regK <- mlreg("K")
-summary(regK[[1]])
-regK[[4]]
-
-preds <- rbind(regE[[3]],
-               regF[[3]],
-               regG[[3]],
-               regK[[3]])
-
-missing_groups <- setdiff(codes$code, unique(survey_raw$group))
+missing_groups <- setdiff(codes$code, unique(harvest_raw$group))
 missing_col <- function(dat, missing){
   for(i in 1:length(missing)) dat[[missing[[i]]]] <- rep(NA, dim(dat)[1])
   dat[order(colnames(dat))]
 }
 
-as_expand <-
-  dplyr::left_join(survey_raw, preds, by = c("year", "trib")) %>%
+Ha_expand <-
+  dplyr::left_join(harvest_raw, preds, by = c("year", "trib")) %>%
   dplyr::filter(!is.na(count)) %>%
   dplyr::group_by(year, group) %>%
   dplyr::summarise(sum_count = sum(count),
@@ -96,8 +90,8 @@ as_expand <-
   tidyr::spread(group, total) %>%
   missing_col(missing_groups)
 
-as_complete <-
-  survey_raw %>%
+Ha_complete <-
+  harvest_raw %>%
   dplyr::group_by(year, group) %>%
   dplyr::summarise(sum_count = sum(count)) %>%
   dplyr::filter(!is.na(sum_count)) %>% 
@@ -105,15 +99,15 @@ as_complete <-
   tidyr::spread(group, total) %>%
   missing_col(missing_groups)
 
-sum(is.na(as_complete))
-sum(is.na(as_expand))
-prod(dim(as_expand[, -1]))
+sum(is.na(Ha_complete))
+sum(is.na(Ha_expand))
+prod(dim(Ha_expand[, -1]))
 
-devtools::use_data(as_complete, pkg = ".\\SusitnaEG", overwrite = TRUE)
-devtools::use_data(as_expand, pkg = ".\\SusitnaEG", overwrite = TRUE)
+devtools::use_data(Ha_complete, pkg = ".\\SusitnaEG", overwrite = TRUE)
+devtools::use_data(Ha_expand, pkg = ".\\SusitnaEG", overwrite = TRUE)
 
 WriteXLS::WriteXLS(
-  c("codes", "as_complete", "as_expand"),
-  ".\\SusitnaEG\\data-raw\\surveydat_for_steve.xlsx", 
-  SheetNames = c("location codes", "complete as", "expanded as"),
+  c("codes", "Ha_complete", "Ha_expand"),
+  ".\\SusitnaEG\\data-raw\\harvestdat_for_steve.xlsx", 
+  SheetNames = c("location codes", "complete Ha", "expanded Ha"),
   na = "NA")
